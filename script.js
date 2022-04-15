1e2963def1f5503f7e4b82ed8a27d49936d5af7c
NDook <thomasmludek@gmail.com>
2022-04-15 14:04:59 +0200

    create new files

---
+++ script.js
@@ -0,0 +1,8 @@
+const message =
+  "Thank you for submitting your message. We will respond as soon as possible.";
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
